a44a3f6285eb8c77976344ec5713316010e8d9b7
piratf <piratf@qq.com>
2023-04-21 11:23:09 +0800

    feat(group_by): show different approach to write singleton in group_by example

--- src/example_main.cpp
+++ src/example_main.cpp
@@ -1,12 +1,24 @@
 #include "ecs_world.h"
 #include <iostream>
 
-int main() {
-  flecs::world world;
-  std::cout << "start init world." << std::endl;
-  ecs_world::InitWorld(world, TestParameters(3, 5));
-  std::cout << "init world finish." << std::endl;
+void InitSystemWithSingletonComponent(flecs::world & world) {
+  world.system<EcsActor, GlobalCounter>().term_at(2).singleton().group_by<EcsRelationBelongsToCell>().write<
+      GlobalCounter>().kind(flecs::OnUpdate).iter([](
+      flecs::iter &it,
+      EcsActor *actors, GlobalCounter* global_counter) {
+    auto group = it.world().entity(it.group_id());
+    std::cout << " - group " << group.path() << ": table [" << it.table().str() << "]\n";
 
+    for (auto i : it) {
+      std::cout << "     {" << actors[i].guid << ", " << actors[i].is_active << "}\n";
+      ++global_counter->calc_actors;
+    }
+
+    std::cout << "\n";
+  });
+}
+
+void GetMutSingletonInsideSystem(flecs::world & world) {
   world.system<EcsActor>().group_by<EcsRelationBelongsToCell>().write<GlobalCounter>().kind(flecs::OnUpdate).iter([](
       flecs::iter &it,
       EcsActor *actors) {
@@ -21,6 +33,50 @@ int main() {
 
     std::cout << "\n";
   });
+}
+
+int main() {
+  flecs::world world;
+  std::cout << "start init world." << std::endl;
+  ecs_world::InitWorld(world, TestParameters(2, 3));
+  std::cout << "init world finish." << std::endl;
+
+//  InitSystemWithSingletonComponent(world);
+  // outputs:
+  // - group ::542: table [EcsActor, (EcsRelationBelongsToCell,542)]
+  //     {0, 0}
+  //
+  // - group ::542: table [EcsActor, (EcsRelationBelongsToCell,542)]
+  //     {1, 0}
+  //
+  // - group ::542: table [EcsActor, (EcsRelationBelongsToCell,542)]
+  //     {2, 0}
+  //
+  // - group ::543: table [EcsActor, (EcsRelationBelongsToCell,543)]
+  //     {10000, 0}
+  //
+  // - group ::543: table [EcsActor, (EcsRelationBelongsToCell,543)]
+  //     {10001, 0}
+  //
+  // - group ::543: table [EcsActor, (EcsRelationBelongsToCell,543)]
+  //     {10002, 0}
+  //
+  //global counter: 6
+
+  GetMutSingletonInsideSystem(world);
+  // outputs:
+  // - group ::542: table [EcsActor, (EcsRelationBelongsToCell,542)]
+  //     {0, 0}
+  //     {1, 0}
+  //     {2, 0}
+  //
+  // - group ::543: table [EcsActor, (EcsRelationBelongsToCell,543)]
+  //     {10000, 0}
+  //     {10001, 0}
+  //     {10002, 0}
+  //
+  //global counter: 6
+
   world.progress();
   std::cout << "global counter: " << world.get<GlobalCounter>()->calc_actors << std::endl;
   return 0;
